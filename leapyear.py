d40ab54ece77359dca43a36b1a8faf2cf70bdd4d
ling <l.ling@Ls-MacBook-Air.local>
2026-03-31 18:18:42 +0700

    tuesday

--- leapyear.py
+++ leapyear.py
@@ -1,13 +1,12 @@
-
+#solution for https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
 def is_leap(year):
     leap=False
-
-
-
-
-
-
-
-
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+    return leap
 year=int(input())
 print(is_leap(year))
